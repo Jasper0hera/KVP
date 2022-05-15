3c58da6e7cefc06cf67358d25e93e3d6b435c1a2
Jasper0hera <105572941+Jasper0hera@users.noreply.github.com>
2022-05-15 10:32:45 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+rm -r l7.py
+rm -r start.sh
+wget https://raw.githubusercontent.com/KLVSPL/STRESSER/main/l7.py
+wget https://raw.githubusercontent.com/KLVSPL/STRESSER/main/start.sh
+chmod +x ./start.sh

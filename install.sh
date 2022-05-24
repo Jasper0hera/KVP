1fdfad1a40fc887d061bf2966d3e0820f1d6274f
Jasper0hera <105572941+Jasper0hera@users.noreply.github.com>
2022-05-24 10:01:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 rm -r l7.py
+rm -r l4.py
+rm -r cc.py
+rm -r s4.py
 rm -r start.sh
+wget https://raw.githubusercontent.com/KLVSPL/STRESSER/main/cc.py
+wget https://raw.githubusercontent.com/KLVSPL/STRESSER/main/l4.py
 wget https://raw.githubusercontent.com/KLVSPL/STRESSER/main/l7.py
+wget https://raw.githubusercontent.com/KLVSPL/STRESSER/main/s4.py
 wget https://raw.githubusercontent.com/KLVSPL/STRESSER/main/start.sh
 chmod +x ./start.sh
